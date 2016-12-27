6110f60dd29c5d56f2b7f29d0b15ea51b875115f
Chris Allison <chris.charles.allison@gmail.com>
2016-12-27 12:20:35 +0000

    added password field to user table

--- villagehall.sql
+++ villagehall.sql
@@ -44,14 +44,13 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(127) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(127) NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `address1` varchar(127) NOT NULL,
-  `address2` varchar(127) NOT NULL,
-  `town` varchar(127) NOT NULL,
-  `postcode` varchar(10) NOT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `address1` varchar(127) DEFAULT NULL,
+  `address2` varchar(127) DEFAULT NULL,
+  `town` varchar(127) DEFAULT NULL,
+  `postcode` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 -- 2016-12-18 10:52:15
