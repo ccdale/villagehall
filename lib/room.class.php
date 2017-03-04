8ec82250f21de4e451d918c539a1bb5414f0ac9f
Chris Allison <chris.charles.allison@gmail.com>
2017-03-04 18:43:52 +0000

    added room.class.php

---
+++ lib/room.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker:
+ *
+ * room.class.php
+ *
+ * Copyright (c) 2017 Chris Allison chris.charles.allison+vh@gmail.com
+ *
+ * This file is part of villagehall.
+ * 
+ * villagehall is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * villagehall is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with villagehall.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Started: Saturday 11 February 2017, 10:19:15
+ * Last Modified: Tuesday 15 April 2014, 07:27:19
+ */
+
+require_once "data.class.php";
+
+class Hall extends Data
+{
+    public function __construct($logg=false,$db=false,$name=false)/*{{{*/
+    {
+        parent::__construct($logg,$db,"room","name",$name);
+    }/*}}}*/
+    public function __destruct()/*{{{*/
+    {
+        parent::__destruct();
+    }/*}}}*/
+}
+?>
