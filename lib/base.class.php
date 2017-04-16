140dd8d524590362b68adb77ee9308395ccd16ed
Chris Allison <chris.charles.allison@gmail.com>
2017-04-16 10:12:04 +0100

    added GP stuff to base

--- lib/base.class.php
+++ lib/base.class.php
@@ -6,7 +6,7 @@
  * base.class.php
  *
  * Started: Friday 24 May 2013, 23:41:08
- * Last Modified: Sunday 26 March 2017, 08:57:52
+ * Last Modified: Sunday 16 April 2017, 10:11:50
  *
  * Copyright (c) 2016 Chris Allison chris.charles.allison+vh@gmail.com
  *
@@ -217,5 +217,105 @@ class Base
       return $path . "/";
     }
   } /*}}}*/
+  /* get and post variables */
+  /** // {{{ funtion GP($var,$trusted=false)
+   * returns the contents of the GET or POST variable
+   * favours POST over GET
+   */
+  public function GP($var,$trusted=false)
+  {
+    $op=false;
+    if(isset($_GET[$var])){
+      $op=$_GET[$var];
+    }
+    if(isset($_POST[$var]))
+    {
+      $op=$_POST[$var];
+    }
+    if(is_string($op))
+    {
+      $op=trim(urldecode($op));
+      if(!$trusted){
+        $op=htmlspecialchars($op,ENT_QUOTES);
+      }
+      // $op=addslashes($op);
+    }
+    return $op;
+  } // }}}
+  /** // {{{ function getDefaultInt($var,$default)
+   * returns the contents of the GET or POST variable
+   * as an int, or the default value if it isn't set
+   */
+  public function getDefaultInt($var,$default)
+  {
+    $op=false;
+    if(false!==($tmp=GP($var))){
+      $tmp=intval($tmp);
+      if($tmp>0){
+        $op=$tmp;
+      }else{
+        $op=$default;
+      }
+    }else{
+      $op=$default;
+    }
+    return $op;
+  } // }}}
+  /* // {{{ function GPA($namearr)
+   * returns an array containing 
+   * selected entries from the $_GET and $_POST arrays
+   * the selected keys are in $namearr
+   */
+  public function GPA($namearr)
+  {
+    $arr=false;
+    if(is_array($namearr) && count($namearr))
+    {
+      reset($namearr);
+      while(list(,$v)=each($namearr))
+      {
+        $arr[$v]=GP($v);
+      }
+    }else{
+      if(is_string($namearr))
+      {
+        $arr[$namearr]=GP($namearr);
+      }
+    }
+    return $arr;
+  } // }}}
+  public function GPAll($trusted=false) // {{{
+  {
+    $op=array();
+    $arr=array_merge($_GET,$_POST);
+    foreach($arr as $key=>$val){
+      if(is_string($val)){
+        $op[$key]=trim(urldecode($val));
+        if(!$trusted){
+          $op[$key]=htmlspecialchars($op[$key],ENT_QUOTES);
+        }
+      }
+    }
+    return $op;
+  } // }}}
+  public function GPType($var,$type="string",$trusted=false)/*{{{*/
+  {
+    $op=GP($var,$trusted);
+    switch($type){
+    case "int":
+      $op=intval($op);
+      break;
+    case "string":
+      $op="$op";
+    }
+    return $op;
+  }/*}}}*/
+  /* php sessions */
+  public function resetPHPSession()/*{{{*/
+  {
+    session_unset();
+    session_destroy();
+    session_start();
+  }/*}}}*/
 }
 ?>
