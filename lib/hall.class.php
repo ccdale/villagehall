6a7568a2ac504f32447e1620c814d3acf7ef8626
Chris Allison <chris.charles.allison@gmail.com>
2017-08-12 12:07:13 +0100

    clean up commented out code

--- lib/hall.class.php
+++ lib/hall.class.php
@@ -53,23 +53,6 @@ class Hall extends Data
             $this->warning("Failed to set the hall from name: $tmphallname");
             return false;
         }
-        /*
-        $sql="select * from hall where name like '" . $this->db->escape($tmphallname) . "'";
-        $arr=$this->db->arrayQuery($sql);
-        if(false!==($junk=$this->ValidArray($arr))){
-            if(isset($arr[0]) && isset($arr[0]["id"]) && isset($arr[0]["name"])){
-                $this->debug("Found hall: " . $arr[0]["name"] . " from server address.");
-                $this->data=$arr[0];
-                $this->id=$this->data["id"];
-                unset($this->data["id"]);
-                $this->readRooms();
-            }else{
-                $this->warning("Did not receive complete information about the hall: $tmphallname from db.");
-            }
-        }else{
-            $this->warning("Failed to find the correct hall from server address: $tmphallname");
-        }
-         */
     }/*}}}*/
     public function numRooms()/*{{{*/
     {
